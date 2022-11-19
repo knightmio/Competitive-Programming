68b8c9b1be7845cedf68d38d589c3617ac3b92cb
knightmio <1094801386@qq.com>
2022-11-19 23:47:34 +1030

    Codeforces Round #834 (Div. 3)

--- CF/1759/G/G/G.cpp
+++ CF/1759/G/G/G.cpp
@@ -9,15 +9,36 @@ using namespace std;
 #define rep(i,a,b) for(int i=(a);i<=(b);++i)
 #define all(_obj) _obj.begin(), _obj.end()
 
-int main()
-{
-    int t;
-    cin >> t;
-    rep(i, 0, t - 1) {
-        int n;
-        cin >> n;
-        vector<int> b(n / 2);
-        rep(j, 0, n / 2 - 1) cin >> b[j];
-
+bool solve() {
+    int n;
+    cin >> n;
+    vector<int> res(n);
+    set<int> st;
+    for (int i = 1; i <= n; i++) st.insert(i);
+    bool b = true;
+    for (int i = 0; i < n / 2; i++) {
+        cin >> res[i*2+1];
+        if (!st.count(res[i * 2 + 1])) b = false;
+        st.erase(res[i * 2 + 1]);
     }
+    if (!b) return false;
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        auto pos = st.lower_bound(res[i*2+1]);
+        if (pos == st.begin()) return false;
+        res[i * 2] = *prev(pos);
+        st.erase(prev(pos));
+    }
+    for (int i = 0; i < n; i++) cout << res[i] << ' ';
+    cout << '\n';
+    return true;
+}
+
+int main() {
+    std::ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        if (!solve()) cout << -1 << '\n';
+    }
+    return 0;
 }
